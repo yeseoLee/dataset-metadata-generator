6e74614d74fe5781a2d53d9d35049dccd9767586
이예서 <49704047+yeseoLee@users.noreply.github.com>
2024-10-30 00:14:34 +0900

    refector: 의존성 requirements로 이동

--- drive_manager.py
+++ drive_manager.py
@@ -1,5 +1,3 @@
-# pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib pyyaml
-
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
